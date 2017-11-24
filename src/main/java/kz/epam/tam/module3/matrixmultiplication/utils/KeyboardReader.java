47c27525ccb1ea1f7cff6a38dede22158d7f7beb
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-11-24 20:16:04 +0600

    renamed reader

--- src/main/java/kz/epam/tam/module3/matrixmultiplication/utils/KeyboardReader.java
+++ src/main/java/kz/epam/tam/module3/matrixmultiplication/utils/KeyboardReader.java
@@ -3,10 +3,11 @@ package kz.epam.tam.module3.matrixmultiplication.utils;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class KeyboardReaderMatrix {
+class KeyboardReader {
+
     private static Scanner scanner = new Scanner(System.in);
 
-    public static int readInt() {
+    static int readInt() {
         while (true) {
             try {
                 return scanner.nextInt();

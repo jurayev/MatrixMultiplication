9213e9fccd349aa2c2027eb69ba157ccbcd44612
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-11-24 20:15:38 +0600

    changed method call

--- src/main/java/kz/epam/tam/module3/matrixmultiplication/runner/Runner.java
+++ src/main/java/kz/epam/tam/module3/matrixmultiplication/runner/Runner.java
@@ -1,11 +1,12 @@
 package kz.epam.tam.module3.matrixmultiplication.runner;
 
-import kz.epam.tam.module3.matrixmultiplication.utils.MatrixExecutor;
+import kz.epam.tam.module3.matrixmultiplication.workflow.MatrixExecutor;
 
 public class Runner {
     public static void main (String[] arg) {
 
-        MatrixExecutor.multiplicateMatrices();
+        MatrixExecutor executor = new MatrixExecutor();
+        executor.multiplicateMatrices();
     }
 }
 
